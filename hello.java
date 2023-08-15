7fe88e820fc2a41df651658f092b6247b757ec45
annabe <xyz@xyz.de>
2023-08-15 13:42:28 +0200

    Weitere Änderung Hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
-      Gruessen gruessen = new Gruessen("Annabel");
+      Gruessen gruessen = new Gruessen("Annabell");
       gruessen.gruesse();
    }
 }
